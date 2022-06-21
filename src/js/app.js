8c8fea805af6404df2623b12b869a80f34d6337d
amidzgz <bendhiafamid@gmail.com>
2022-06-21 22:09:36 +0000

    ejercicio hecho + enlaces en otra pestaña

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}y"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${variables.twitter}" target="_blank"><i  class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
