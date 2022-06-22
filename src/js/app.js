0ab5f5f127a3b2b99e98ef2e260041edae77518c
amidzgz <bendhiafamid@gmail.com>
2022-06-22 20:20:38 +0000

    añadidos ternarios

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <h1>${variables.name != null ? variables.name : "Nombre"} ${
+    variables.lastname != null ? variables.lastname : "Apellido"
+  }</h1>
+          <h2>${variables.role != null ? variables.role : "Rol"}</h2>
+          <h3>${variables.city != null ? variables.city : "Ciudad"}, ${
+    variables.country != null ? variables.country : "País"
+  }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${variables.twitter}" target="_blank"><i  class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}" target="_blank"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${
+              variables.twitter
+            }" target="_blank"><i  class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+            }" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
